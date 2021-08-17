5b03760df34733c8a34cef3596a3f40e6cf84f12
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2021-08-16 20:29:14 -0400

    Add models to ClientController endpoint

---
+++ Models/Responses/ClientResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientsAdmin.API.Models.Responses
+{
+    public class ClientResponse
+    {
+        public int Id { get; set; }
+        public string SocialReason { get; set; }
+        public string ComercialName { get; set; }
+        public string Rnc { get; set; }
+        public string Phone { get; set; }
+        public int Adresses { get; set; }
+    }
+}
